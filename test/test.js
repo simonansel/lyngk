f9bf7b89418588ba68a02b3a973db4aabddda8c7
simonansel <contact@simon-ansel.fr>
2017-09-29 14:21:45 +0200

    add test to first story

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.testStory1 = function() {
+    var coordinates = new Lyngk.Coordinates('A', 1);
+
+    assertFalse(coordinates.is_valid());
+}
