b27f48a5d1f0c85f948d0a799fcf7d043f9d8039
simonansel <contact@simon-ansel.fr>
2017-09-29 15:31:42 +0200

    add second scenario

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,23 +1,20 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
-    const length = 9;
-    this.is_valid = function () {
-        var lyngk = new Array(length);
-        lyngk[0] = new Array(["C"]);
-        lyngk[1] = new Array(["B", "C", "D", "E"]);
-        lyngk[2] = new Array(["A", "B", "C", "D", "E", "F", "G"]);
-        lyngk[3] = new Array(["B", "C", "D", "E", "F", "G"]);
-        lyngk[4] = new Array(["B", "C", "D", "E", "F", "G", "H"]);
-        lyngk[5] = new Array(["C", "D", "E", "F", "G", "H"]);
-        lyngk[6] = new Array(["C", "D", "E", "F", "G", "H", "I"]);
-        lyngk[7] = new Array(["E", "F", "G", "H"]);
-        lyngk[8] = new Array(["G"]);
 
-        if (lyngk[l - 1].indexOf(c) != -1)
-            return true;
-        else
-            return false;
+    this.is_valid = function () {
+        var lyngk = [];
+        lyngk[0] = ["C"];
+        lyngk[1] = ["B", "C", "D", "E"];
+        lyngk[2] = ["A", "B", "C", "D", "E", "F", "G"];
+        lyngk[3] = ["B", "C", "D", "E", "F", "G"];
+        lyngk[4] = ["B", "C", "D", "E", "F", "G", "H"];
+        lyngk[5] = ["C", "D", "E", "F", "G", "H"];
+        lyngk[6] = ["C", "D", "E", "F", "G", "H", "I"];
+        lyngk[7] = ["E", "F", "G", "H"];
+        lyngk[8] = ["G"];
+
+        return lyngk[l - 1].indexOf(c) !== -1;
     }
 
 };
--- test/test.js
+++ test/test.js
@@ -6,4 +6,22 @@ LyngkTestCase.prototype.testStory1 = function () {
     var coordinates = new Lyngk.Coordinates('A', 1);
 
     assertFalse(coordinates.is_valid());
-}
+};
+
+LyngkTestCase.prototype.testStory2 = function () {
+    var tabLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+    var tabNumber = [1,2,3,4,5,6,7,8,9];
+
+    var nbGoodCoordinates = 0;
+
+    for(var i = 0; i < tabLetter.length ; i++){
+        for(var j = 0; j < tabNumber.length ; j++){
+            var coordinates = new Lyngk.Coordinates(tabLetter[i], tabNumber[j]);
+
+            if(coordinates.is_valid())
+                nbGoodCoordinates++;
+        }
+    }
+
+    assertEquals(43, nbGoodCoordinates);
+};
